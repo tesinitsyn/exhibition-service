6988696473d29678734573a2ba347945de2c377d
tesinitsyn <sitiev174@gmail.com>
2024-12-10 21:39:32 +0300

    initial commit

---
+++ src/main/java/com/tesinitsyn/exhibitionservice/ExhibitionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.exhibitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExhibitionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tesinitsyn/exhibitionservice/ExhibitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.exhibitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
